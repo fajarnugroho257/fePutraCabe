038a3dd3846a0909065482b3f2620e0128601ae0
fajarnugroho257 <fajarnugroho274@gmail.com>
2026-01-06 14:18:21 +0700

    image

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -45,7 +45,7 @@ function Login() {
       <div className="hidden bg-colorBlue md:block">
         <div className="h-screen flex items-center justify-center">
           <div>
-            <img src={putraCabe} alt="putraCabe" className="w-56 mx-auto" />
+            <img src='/putraCabe.png' alt="putraCabe" className="w-56 mx-auto" />
             <h4 className="text-colorGray font-poppins text-4xl font-semibold text-center">
               Putra Cabe
             </h4>
